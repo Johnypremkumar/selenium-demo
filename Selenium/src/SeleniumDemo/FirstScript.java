65f2200536cbc0f90d47c286051a29880d2b2a6b
Premkumar, Johny <Johny.Premkumar@xpo.com>
2020-06-04 17:17:16 +0530

    adding selenium test script

---
+++ Selenium/src/SeleniumDemo/FirstScript.java
@@ -0,0 +1,24 @@
+package SeleniumDemo;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstScript {
+
+	public static void main(String[] args) throws InterruptedException{
+		System.setProperty("webdriver.chrome.driver", "D:\\Johny GIT Handson\\selenium\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("https://login.yahoo.com/");
+		driver.findElement(By.xpath("//input[@id='login-username']")).sendKeys("johnytest@yahoo.com");
+		Thread.sleep(10000);
+		driver.findElement(By.xpath("//input[@id='login-signin']")).click();
+	}
+	
+}
